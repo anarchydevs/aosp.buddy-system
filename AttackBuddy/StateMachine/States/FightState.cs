1a06819189500ccf7b2d4f0808f18b5ea6f56763
Knowidea <Mellowclads@hotmail.com>
2023-03-29 14:01:22 -0500

    removed mirror of xan for maiden

--- AttackBuddy/StateMachine/States/FightState.cs
+++ AttackBuddy/StateMachine/States/FightState.cs
@@ -78,8 +78,8 @@ namespace AttackBuddy
                 if (Extensions.CanAttack())
                 {
                     if (_target.Buffs.Contains(253953) == false
-                        && _target.Buffs.Contains(NanoLine.ShovelBuffs) == false
-                        && _target.Buffs.Contains(302745) == false
+                       // && _target.Buffs.Contains(NanoLine.ShovelBuffs) == false
+                        //&& _target.Buffs.Contains(302745) == false
                         && _target.IsPlayer == false) 
                     {
                         if (_target.Position.DistanceFrom(DynelManager.LocalPlayer.Position) <= AttackBuddy.Config.CharSettings[Game.ClientInst].AttackRange)
