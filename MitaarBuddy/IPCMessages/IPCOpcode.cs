fe5ecf33b2edc7cbf65a072380ac33b0d4fab768
Knowidea <Mellowclads@hotmail.com>
2023-03-30 14:33:16 -0500

    added MitaarBuddy still WIP

---
+++ MitaarBuddy/IPCMessages/IPCOpcode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MitaarBuddy.IPCMessages
+{
+    public enum IPCOpcode
+    {
+        Start = 1011,
+        Stop = 1012
+    }
+}
