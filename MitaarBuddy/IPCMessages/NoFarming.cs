9f32dfb877f8f1e64c76aaab189ec59ff8471a12
Knowidea <Mellowclads@hotmail.com>
2023-04-05 15:22:53 -0500

    Added MitaarBuddy

---
+++ MitaarBuddy/IPCMessages/NoFarming.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using AOSharp.Core.IPC;
+using SmokeLounge.AOtomation.Messaging.Serialization.MappingAttributes;
+
+namespace MitaarBuddy.IPCMessages
+{
+    [AoContract((int)IPCOpcode.NoFarming)]
+    public class NoFarmingMessage : IPCMessage
+    {
+        public override short Opcode => (short)IPCOpcode.NoFarming;
+    }
+}
---
+++ MitaarBuddy/StateMachine/States/DiedState.cs
@@ -0,0 +1,64 @@
+﻿using AOSharp.Common.GameData;
+using AOSharp.Core;
+using AOSharp.Core.Movement;
+using AOSharp.Core.UI;
+using AOSharp.Pathfinding;
+using MitaarBuddy.IPCMessages;
+using SmokeLounge.AOtomation.Messaging.Messages.N3Messages.OrgServerMessages;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MitaarBuddy
+{
+    public class DiedState : IState
+    {
+        public IState GetNextState()
+        {
+
+            //if (DynelManager.LocalPlayer.Position.DistanceFrom(Constants._reclaim) < 10.0f)
+            //{
+            //    MovementController.Instance.SetPath(Constants._pathToMitaar);
+            //}
+
+            if ((DynelManager.LocalPlayer.Position.DistanceFrom(Constants._entrance) < 20.0f)
+                && Extensions.CanProceed())
+                return new EnterState();
+
+            return null;
+        }
+
+        public void OnStateEnter()
+        {
+            MitaarBuddy._died = true;
+
+            Chat.WriteLine($"DiedState::OnStateEnter");
+        }
+
+        public void OnStateExit()
+        {
+            Chat.WriteLine("DiedState::OnStateExit");
+        }
+
+        public void Tick()
+        {
+            if (Game.IsZoning) { return; }
+
+
+
+            if (DynelManager.LocalPlayer.MovementState == MovementState.Sit
+                && DynelManager.LocalPlayer.HealthPercent > 65 && DynelManager.LocalPlayer.NanoPercent > 65)
+                MitaarBuddy.NavMeshMovementController.SetMovement(MovementAction.LeaveSit);
+
+            if (DynelManager.LocalPlayer.HealthPercent > 65 && DynelManager.LocalPlayer.NanoPercent > 65
+                && DynelManager.LocalPlayer.GetStat(Stat.TemporarySkillReduction) <= 1
+                && DynelManager.LocalPlayer.MovementState != MovementState.Sit
+                && Playfield.ModelIdentity.Instance == Constants.XanHubId && !MitaarBuddy.NavMeshMovementController.IsNavigating)
+                MovementController.Instance.SetPath(Constants._pathToMitaar); ;
+        }
+    }
+}
