881894289241bff1df412406950feef7968a7649
Knowidea <Mellowclads@hotmail.com>
2023-05-08 13:17:56 -0500

    added vortexx buddy

---
+++ VortexxBuddy/StateMachine/States/ImmunityState.cs
@@ -0,0 +1,170 @@
+﻿using AOSharp.Common.GameData;
+using AOSharp.Core;
+using AOSharp.Core.Inventory;
+using AOSharp.Core.Movement;
+using AOSharp.Core.UI;
+using AOSharp.Pathfinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace VortexxBuddy
+{
+    public class ImmunityState : IState
+    {
+
+        private static SimpleChar _vortexx;
+        private static SimpleChar _releasedSpirit;
+
+        private static double _timer;
+
+        public IState GetNextState()
+        {
+            _releasedSpirit = DynelManager.NPCs
+                  .Where(c => c.Health > 0
+                         && c.Name.Contains("Released Spirit"))
+                     .FirstOrDefault();
+
+            if (Playfield.ModelIdentity.Instance == Constants.VortexxId
+                 && _releasedSpirit == null)
+                return new FightState();
+
+            return null;
+        }
+
+        public void OnStateEnter()
+        {
+            Chat.WriteLine("ImmunityState");
+        }
+
+        public void OnStateExit()
+        {
+            Chat.WriteLine("Exit ImmunityState");
+        }
+
+        public void Tick()
+        {
+            if (!Team.IsInTeam || Game.IsZoning) { return; }
+
+            foreach (TeamMember member in Team.Members)
+            {
+                if (!ReformState._teamCache.Contains(member.Identity))
+                    ReformState._teamCache.Add(member.Identity);
+            }
+
+            if (Playfield.ModelIdentity.Instance == Constants.VortexxId)
+            {
+
+                _vortexx = DynelManager.NPCs
+                 .Where(c => c.Health > 0
+                  && c.Name.Contains("Ground Chief Vortexx"))
+                  .FirstOrDefault();
+
+                _releasedSpirit = DynelManager.NPCs
+                   .Where(c => c.Health > 0
+                          && c.Name.Contains("Released Spirit"))
+                      .FirstOrDefault();
+
+               
+                if (DynelManager.LocalPlayer.Position.DistanceFrom(Constants._centerPodium) > 5f
+                    && !MovementController.Instance.IsNavigating)
+                    VortexxBuddy.NavMeshMovementController.SetNavMeshDestination(Constants._centerPodium);
+
+                if (VortexxBuddy._settings["Immunity"].AsBool() && _releasedSpirit != null)
+                    if (DynelManager.LocalPlayer.FightingTarget != null
+                       && DynelManager.LocalPlayer.FightingTarget.Name == _vortexx.Name)
+                    {
+                        DynelManager.LocalPlayer.StopAttack();
+                    }
+
+                if (_releasedSpirit != null)
+                {
+                    if (_releasedSpirit.Position.DistanceFrom(Constants._redPodium) < 5)
+                    {
+                        if (!VortexxBuddy._red)
+                        {
+                            Task.Factory.StartNew(async () =>{
+                                Item red = Inventory.Items.Where(x => ImmunityCrystals.BloodRedNotumCrystal.Contains(x.Id)).FirstOrDefault();
+
+                                await Task.Delay(10000);
+
+                                if (!Item.HasPendingUse)
+                                    red.Use();
+
+                            });
+                        }
+                        VortexxBuddy._red = true;
+                    }
+
+                    if (_releasedSpirit.Position.DistanceFrom(Constants._greenPodium) < 5)
+                    {
+                        if (!VortexxBuddy._green)
+                        {
+                            Task.Factory.StartNew(async () => {
+                                Item green = Inventory.Items.Where(x => ImmunityCrystals.PulsatingGreenNotumCrystal.Contains(x.Id)).FirstOrDefault();
+
+                                await Task.Delay(10000);
+
+                                if (!Item.HasPendingUse)
+                                    green.Use();
+
+                            });
+                        }
+                        VortexxBuddy._green = true;
+                    }
+
+                    if (_releasedSpirit.Position.DistanceFrom(Constants._yellowPodium) < 5)
+                    {
+                        if (!VortexxBuddy._yellow)
+                        {
+                            Task.Factory.StartNew(async () => {
+                                Item yellow = Inventory.Items.Where(x => ImmunityCrystals.GoldenNotumCrystal.Contains(x.Id)).FirstOrDefault(); 
+
+                                await Task.Delay(10000);
+
+                                if (!Item.HasPendingUse)
+                                    yellow.Use();
+
+                            });
+                        }
+                        VortexxBuddy._yellow = true;
+                    }
+
+                    if (_releasedSpirit.Position.DistanceFrom(Constants._bluePodium) < 5)
+                    {
+                        if (!VortexxBuddy._blue)
+                        {
+                            Task.Factory.StartNew(async () => {
+                                Item blue = Inventory.Items.Where(x => ImmunityCrystals.CobaltBlueNotumCrystal.Contains(x.Id)).FirstOrDefault();
+
+                                await Task.Delay(10000);
+
+                                if (!Item.HasPendingUse)
+                                    blue.Use();
+
+                            });
+                        }
+                        VortexxBuddy._blue = true;
+                    }
+
+                   
+                }
+            }
+        }
+
+        public static class ImmunityCrystals
+        {
+            public static readonly int[] BloodRedNotumCrystal = {280581};
+            public static readonly int[] PulsatingGreenNotumCrystal = {280585};
+            public static readonly int[] GoldenNotumCrystal = {280586};
+            public static readonly int[] CobaltBlueNotumCrystal = {280584};
+
+           
+
+
+        }
+    }
+}
---
+++ VortexxBuddy/StateMachine/States/ReformState.cs
@@ -0,0 +1,144 @@
+﻿using AOSharp.Common.GameData;
+using AOSharp.Core;
+using AOSharp.Core.Movement;
+using AOSharp.Core.UI;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VortexxBuddy
+{
+    public class ReformState : IState
+    {
+        private const float ReformTimeout = 100;
+        private const float DisbandDelay = 10;
+        private const float InviteDelay = 13;
+
+        private static double _reformStartedTime;
+        private double _inviting;
+
+        private ReformPhase _phase;
+
+        public static List<Identity> _teamCache = new List<Identity>();
+        private static List<Identity> _invitedList = new List<Identity>();
+
+        private static bool _init = false;
+
+        public IState GetNextState()
+        {
+            if (Extensions.TimedOut(_reformStartedTime, ReformTimeout))
+                return new IdleState();
+
+            if (_phase == ReformPhase.Completed)
+            {
+                if (Team.Members.Where(c => c.Character != null).ToList().Count == _teamCache.Count())
+                    return new IdleState();
+            }
+
+            return null;
+        }
+
+        public void OnStateEnter()
+        {
+            //Chat.WriteLine("Reforming");
+
+            _reformStartedTime = Time.NormalTime;
+
+            VortexxBuddy.NavMeshMovementController.SetNavMeshDestination(Constants._startPos);
+
+            if (DynelManager.LocalPlayer.Identity != VortexxBuddy.Leader)
+            {
+                Team.TeamRequest += OnTeamRequest;
+                _phase = ReformPhase.Waiting;
+            }
+
+            VortexxBuddy._clearToEnter = false;
+            FarmingState._initCorpse = false;
+        }
+
+        public void OnStateExit()
+        {
+            //Chat.WriteLine("Done Reforming");
+            _invitedList.Clear();
+            _teamCache.Clear();
+
+            _init = false;
+
+            FarmingState._initCorpse = false;
+
+        }
+
+        public void Tick()
+        {
+            if (Game.IsZoning) { return; }
+
+            if (Team.IsInTeam)
+            {
+                foreach (TeamMember member in Team.Members)
+                {
+                    if (!_teamCache.Contains(member.Identity))
+                        _teamCache.Add(member.Identity);
+                }
+            }
+
+            if (_phase == ReformPhase.Inviting
+                && _invitedList.Count() < _teamCache.Count()
+                && Time.NormalTime > _inviting + DisbandDelay
+                && !_init)
+            {
+                _init = true;
+
+                foreach (SimpleChar player in DynelManager.Players.Where(c => c.IsInPlay && !_invitedList.Contains(c.Identity) && _teamCache.Contains(c.Identity)))
+                {
+                    if (_invitedList.Contains(player.Identity)) { continue; }
+
+                    _invitedList.Add(player.Identity);
+
+                    if (player.Identity == VortexxBuddy.Leader) { continue; }
+
+                    Team.Invite(player.Identity);
+                   // Chat.WriteLine($"Inviting {player.Name}");
+                }
+            }
+
+            if (_phase == ReformPhase.Disbanding)
+            {
+                if (!Team.IsInTeam)
+                {
+                    _inviting = Time.NormalTime;
+                    _phase = ReformPhase.Inviting;
+                    //Chat.WriteLine("ReformPhase.Inviting");
+                }
+                else if (Team.Members.Where(c => c.Character != null && c.Character.IsInPlay).ToList().Count == _teamCache.Count()
+                        || Time.NormalTime > _reformStartedTime + ReformTimeout)
+                    Team.Disband();
+            }
+
+            if (_phase == ReformPhase.Inviting
+                && _invitedList.Count() == _teamCache.Count()
+                && Time.NormalTime > _inviting + DisbandDelay
+                && Team.Members.Where(c => c.Character != null).ToList().Count == _teamCache.Count()
+                && _init)
+            {
+                _phase = ReformPhase.Completed;
+                //Chat.WriteLine("ReformPhase.Completed");
+            }
+        }
+
+        private void OnTeamRequest(object s, TeamRequestEventArgs e)
+        {
+            if (_teamCache.Contains(e.Requester))
+            {
+                _phase = ReformPhase.Completed;
+                e.Accept();
+            }
+        }
+
+        private enum ReformPhase
+        {
+            Disbanding,
+            Inviting,
+            Waiting,
+            Completed
+        }
+    }
+}
