790a998e056c214dfe9829c7b5afb469035d8b2c
Knowidea <Mellowclads@hotmail.com>
2023-11-01 07:28:03 -0500

    fixed some died state issues

--- AXPBuddy/Constants.cs
+++ AXPBuddy/Constants.cs
@@ -12,6 +12,9 @@ namespace AXPBuddy
         public static Vector3 S13GoalPos = new Vector3(170.3f, 6.2f, 486.1f);
 
         public static Vector3 UnicornLever = new Vector3(71.0f, 101.0f, 232.0f);
+        public static Vector3 Bridge = new Vector3(140.4, 101.0, 216.0); 
+
+        public static Vector3 MarkerpPoll = new Vector3(198.3, 0.3, 242.5);
 
         public const int S13Id = 4365;
         public const int APFHubId = 4368;
